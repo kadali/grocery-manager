87520a415d0dc1b74b8f49bfe1023cf7fca879c5
Sravanthi Kadali <sjkadali@gmail.com>
2016-01-02 22:22:23 -0500

    Initial Commit

---
+++ GroceryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroceryApp
+//
+//  Created by Kadali, Sravanthi on 1/2/16.
+//  Copyright © 2016 Sravanthi Kadali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
